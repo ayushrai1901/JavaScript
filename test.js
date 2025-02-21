d34d284e20c32ef11b5d67b0a42cc3e54dc3e0f7
@code_ayush <86945359+ayushrai1901@users.noreply.github.com>
2025-02-21 15:15:15 +0000

    Test

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("Ayush")
+
+const accountId = 12345
+let accountEmail = "ayush@gamail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 //Not allowed if the variable is defined as const
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
